2be45f4bdce7d08ab4e1ca38f334ae663af93538
anagd <andelamo@myuax.es>
2023-10-16 12:49:07 +0200

    Prueba6

--- src/main/java/org/example/CampanasDeGaussFactory.java
+++ src/main/java/org/example/CampanasDeGaussFactory.java
@@ -8,42 +8,34 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class CampanasDeGaussFactory {
-    private static final int numWorkers = 4;
-    private static final int bufferSize = 100;
-    private static final int numBalls = 1000;
-
     public static void main(String[] args) {
-        BlockingQueue<Component> buffer = new ArrayBlockingQueue<>(bufferSize);
-        List<Component> componentList = new ArrayList<>(numBalls);
-
         JFrame frame = new JFrame("Campanas de Gauss Factory");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-
-        JTextArea textArea = new JTextArea();
-        frame.add(new JScrollPane(textArea), BorderLayout.CENTER);
-
-        for (int i = 0; i < numWorkers; i++) {
-            Thread workerThread = new Thread(new Worker("Worker " + i, buffer, numBalls / numWorkers));
-            workerThread.start();
-        }
-
-        Thread assemblerThread = new Thread(new Assembler(buffer));
-        assemblerThread.start();
-
         frame.setSize(800, 600);
+
+        DistributionPanel distributionPanel = new DistributionPanel();
+        frame.add(distributionPanel);
         frame.setVisible(true);
 
-        while (componentList.size() < numBalls) {
+        List<Integer> frequencyData = new ArrayList<>();
+
+        // Crear e iniciar trabajadores
+        for (int i = 0; i < 1000; i++) {
+            // Simular la producción de un componente
+            int component = (int) (Math.random() * 10); // Valores entre 0 y 9
+            frequencyData.add(component);
+
+            // Actualizar el panel de distribución
+            distributionPanel.setFrequencyData(frequencyData);
+            distributionPanel.repaint();
+
             try {
-                Component component = buffer.take();
-                componentList.add(component);
-                textArea.append("Componente producido: " + component + "\n");
-                textArea.setCaretPosition(textArea.getDocument().getLength());
+                Thread.sleep(100);
             } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+                e.printStackTrace();
             }
         }
+
         System.out.println("Todos los componentes han sido producidos");
     }
 }
--- src/main/java/org/example/DistributionPanel.java
+++ src/main/java/org/example/DistributionPanel.java
@@ -1,12 +1,35 @@
 package org.example;
 
 import javax.swing.*;
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
 import java.util.List;
-import java.util.Collections;
 
 
-public class DistributionPanel extends JPanel {
+class DistributionPanel extends JPanel {
+    private List<Integer> frequencyData;
 
+    public void setFrequencyData(List<Integer> frequencyData) {
+        this.frequencyData = frequencyData;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (frequencyData != null) {
+            int barWidth = getWidth() / frequencyData.size();
+            int maxCount = 10; // Valor máximo en este ejemplo
+
+            for (int i = 0; i < frequencyData.size(); i++) {
+                int count = frequencyData.get(i);
+                int barHeight = (int) ((double) count / maxCount * getHeight());
+
+                int x = i * barWidth;
+                int y = getHeight() - barHeight;
+
+                g.setColor(Color.blue);
+                g.fillRect(x, y, barWidth, barHeight);
+            }
+        }
+    }
 }
