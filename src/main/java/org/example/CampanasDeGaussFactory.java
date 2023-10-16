69449543d50403d721cb77d1dcd5bc164c0447f3
anagd <andelamo@myuax.es>
2023-10-16 11:55:21 +0200

    Prueba2

--- src/main/java/org/example/CampanasDeGaussFactory.java
+++ src/main/java/org/example/CampanasDeGaussFactory.java
@@ -3,10 +3,15 @@ package org.example;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class CampanasDeGaussFactory {
     public static void main(String[] args) {
         int numWorkers = 4;
-        int bufferSize = 100;
+        int bufferSize = 10;
+        int maxComponents = 10;
+        int componentsPerWorker = maxComponents / numWorkers;
 
         // Búfer compartido para los componentes producidos
         BlockingQueue<Component> buffer = new ArrayBlockingQueue<>(bufferSize);
@@ -14,17 +19,15 @@ public class CampanasDeGaussFactory {
         // Crear instancias de Runnable para trabajadores y ensamblador
         Runnable[] runnables = new Runnable[numWorkers + 1];
         for (int i = 0; i < numWorkers; i++) {
-            runnables[i] = new Worker("Worker " + i, buffer);
+            runnables[i] = new Worker("Worker " + i, buffer, componentsPerWorker);
         }
         runnables[numWorkers] = new Assembler(buffer);
-
 
         Thread[] threads = new Thread[runnables.length];
         for (int i = 0; i < runnables.length; i++) {
             threads[i] = new Thread(runnables[i]);
             threads[i].start();
         }
-
 
         try {
             for (Thread thread : threads) {
@@ -35,5 +38,10 @@ public class CampanasDeGaussFactory {
         }
 
         System.out.println("Todos los hilos han terminado");
+
+        // Imprimir los componentes producidos
+        for (Component component : buffer) {
+            System.out.println(component);
+        }
     }
 }
--- src/main/java/org/example/Worker.java
+++ src/main/java/org/example/Worker.java
@@ -2,23 +2,32 @@ package org.example;
 
 import java.util.concurrent.BlockingQueue;
 
-class Worker implements Runnable {
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Worker implements Runnable {
     private String name;
     private BlockingQueue<Component> buffer;
+    private int componentsToProduce;
+    private static AtomicInteger uniqueIdCounter = new AtomicInteger(0);
 
-    public Worker(String name, BlockingQueue<Component> buffer) {
+    public Worker(String name, BlockingQueue<Component> buffer, int componentsToProduce) {
         this.name = name;
         this.buffer = buffer;
+        this.componentsToProduce = componentsToProduce;
     }
 
     @Override
     public void run() {
-        while (true) {
+        int producedComponents = 0; // Contador de componentes producidos por este trabajador
+
+        while (producedComponents < componentsToProduce) {
             try {
                 // Simular la producción de un componente
-                Component component = new Component();
+                Component component = new Component(generateUniqueId(), generateDescription());
                 // Colocar el componente en el búfer compartido
                 buffer.put(component);
+                producedComponents++;
                 // Realizar alguna simulación de trabajo
                 Thread.sleep(100);
             } catch (InterruptedException e) {
@@ -26,4 +35,17 @@ class Worker implements Runnable {
             }
         }
     }
+
+    private int generateUniqueId() {
+        // Utiliza un contador atómico para generar IDs únicos
+        return uniqueIdCounter.getAndIncrement();
+    }
+
+    private String generateDescription() {
+        // Genera descripciones aleatorias para los componentes
+        String[] descriptions = {"Component A", "Component B", "Component C", "Component D"};
+        int randomIndex = (int) (Math.random() * descriptions.length);
+        return descriptions[randomIndex];
+    }
 }
+
